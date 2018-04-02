1bad8d6d8e51f72365df89aab8c582e6044a8c09
Joe Yates <joe.g.yates@gmail.com>
2018-04-02 11:00:54 +0200

    Set up systemd

---
+++ resources/systemd.rb
@@ -0,0 +1,44 @@
+resource_name :rails_systemd
+
+actions :create
+default_action :create
+
+property :service, String, name_attribute: true
+property :app_path, String
+property :user, String, default: "deploy"
+property :rails_env, String, default: "production"
+
+action :create do
+  execute "systemctl daemon-reload" do
+    action :nothing
+    command "systemctl daemon-reload"
+  end
+
+  service new_resource.service do
+    action :nothing
+    supports [:start, :stop, :restart, :status]
+  end
+
+  systemd_unit_path = ::File.join(
+    "", "lib", "systemd", "system", "#{new_resource.service}.service"
+  )
+
+  template systemd_unit_path do
+    source "systemd/service.erb"
+    variables(
+      name: new_resource.service,
+      app_path: new_resource.app_path,
+      rails_env: new_resource.rails_env
+    )
+    notifies :run, "execute[systemctl daemon-reload]", :immediately
+  end
+
+  commands = %w(start stop restart status).map do |action|
+    "/bin/systemctl #{action} #{new_resource.service}"
+  end
+
+  file "/etc/sudoers.d/#{new_resource.user}-#{new_resource.service}" do
+    content "#{new_resource.user} ALL=(ALL) NOPASSWD: #{commands.join(", ")}\n"
+    mode 0440
+  end
+end
