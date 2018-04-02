014405e61a57a7e1e352d6cf43d16c12422d0329
Joe Yates <joe.g.yates@gmail.com>
2018-04-02 11:21:37 +0200

    Bugfix: avoid name collision with standard resource

--- resources/systemd.rb
+++ resources/systemd.rb
@@ -3,7 +3,7 @@ resource_name :rails_systemd
 actions :create
 default_action :create
 
-property :service, String, name_attribute: true
+property :name, String, name_attribute: true
 property :app_path, String
 property :user, String, default: "deploy"
 property :rails_env, String, default: "production"
@@ -14,19 +14,19 @@ action :create do
     command "systemctl daemon-reload"
   end
 
-  service new_resource.service do
+  service new_resource.name do
     action :nothing
     supports [:start, :stop, :restart, :status]
   end
 
   systemd_unit_path = ::File.join(
-    "", "lib", "systemd", "system", "#{new_resource.service}.service"
+    "", "lib", "systemd", "system", "#{new_resource.name}.service"
   )
 
   template systemd_unit_path do
     source "systemd/service.erb"
     variables(
-      name: new_resource.service,
+      name: new_resource.name,
       app_path: new_resource.app_path,
       rails_env: new_resource.rails_env
     )
@@ -34,10 +34,10 @@ action :create do
   end
 
   commands = %w(start stop restart status enable disable).map do |action|
-    "/bin/systemctl #{action} #{new_resource.service}"
+    "/bin/systemctl #{action} #{new_resource.name}"
   end
 
-  file "/etc/sudoers.d/#{new_resource.user}-#{new_resource.service}" do
+  file "/etc/sudoers.d/#{new_resource.user}-#{new_resource.name}" do
     content "#{new_resource.user} ALL=(ALL) NOPASSWD: #{commands.join(", ")}\n"
     mode 0440
   end
