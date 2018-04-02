d4e592f19dca760fd21beab076859aab5f8ed304
Joe Yates <joe.g.yates@gmail.com>
2018-04-02 11:00:37 +0200

    Set up ruby

---
+++ resources/ruby.rb
@@ -0,0 +1,17 @@
+resource_name :rails_ruby
+
+actions :create
+default_action :create
+
+property :version, String, name_attribute: true
+property :bundler_version, String, default: nil
+
+action :create do
+  ruby_rbenv_system_install "default"
+  rbenv_ruby new_resource.version
+
+  rbenv_gem "bundler" do
+    version new_resource.bundler_version if new_resource.bundler_version
+    rbenv_version new_resource.version
+  end
+end
