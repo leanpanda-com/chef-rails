8bca48d172b8aa15de8fe9091b79e0d8fe869098
Joe Yates <joe.g.yates@gmail.com>
2018-04-04 16:21:50 +0200

    Logrotate puma, if used

--- resources/app.rb
+++ resources/app.rb
@@ -76,4 +76,18 @@ action :create do
     user new_resource.user
     group new_resource.group
   end
+
+  rails_logrotate log_name_base + "_puma_access" do
+    path ::File.join(log_path_base, "puma_access.log")
+    user new_resource.user
+    group new_resource.group
+    only_if { node["rails"]["server"] == "puma" }
+  end
+
+  rails_logrotate log_name_base + "_puma_error" do
+    path ::File.join(log_path_base, "puma_error.log")
+    user new_resource.user
+    group new_resource.group
+    only_if { node["rails"]["server"] == "puma" }
+  end
 end
